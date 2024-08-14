737352db1fe36582af2d380fddc26c7cdbe7c3fb
Namhyun <jenple9709@gmail.com>
2024-08-14 21:38:14 +0900

    refactor: CreateRoom test코드 변경

--- src/app/(main)/create-room/_component/CrateRoom.test.tsx
+++ src/app/(main)/create-room/_component/CrateRoom.test.tsx
@@ -24,7 +24,7 @@ describe('CreateRoom Component', () => {
     (useRouter as jest.Mock).mockReturnValue({
       push: mockRouterPush,
     });
-    (useAuthStore as jest.Mock).mockReturnValue({
+    (useAuthStore as unknown as jest.Mock).mockReturnValue({
       isLoggedIn: true,
     });
     (usePostToon as jest.Mock).mockReturnValue({
@@ -36,12 +36,12 @@ describe('CreateRoom Component', () => {
     jest.clearAllMocks();
   });
 
-  it('should render the component', () => {
+  it('컴포넌트 렌더링 테스트', () => {
     render(<CreateRoom />);
     expect(screen.getByText('드로잉 주제')).toBeInTheDocument();
   });
 
-  it('should allow the user to input a subject', () => {
+  it('사용자 주제 입력 테스트', () => {
     render(<CreateRoom />);
     const input = screen.getByPlaceholderText(
       '멤버들과 그릴 그림 주제를 입력해주세요!',
@@ -50,21 +50,21 @@ describe('CreateRoom Component', () => {
     expect(input).toHaveValue('테스트 주제');
   });
 
-  it('should update the member option when a member is selected', () => {
+  it('멤버 선택 시 멤버 옵션 업데이트 테스트', () => {
     render(<CreateRoom />);
     const memberRadio = screen.getByLabelText('1명');
     fireEvent.click(memberRadio);
     expect(memberRadio).toBeChecked();
   });
 
-  it('should update the time option when a time is selected', () => {
+  it('시간 선택 시 시간 옵션 업데이트 테스트', () => {
     render(<CreateRoom />);
     const timeRadio = screen.getByLabelText('12초');
     fireEvent.click(timeRadio);
     expect(timeRadio).toBeChecked();
   });
 
-  it('should call the submit function and redirect on successful submission', async () => {
+  it('방 생성 클릭 시 성공적으로 제출 후 리디렉션 테스트', async () => {
     mockMutateAsync.mockResolvedValueOnce({ id: '123' });
     render(<CreateRoom />);
     const submitButton = screen.getByText('시작하기');
@@ -73,8 +73,8 @@ describe('CreateRoom Component', () => {
     expect(mockRouterPush).toHaveBeenCalledWith('/drawing/123?count=1');
   });
 
-  it('should redirect to login if not logged in', () => {
-    (useAuthStore as jest.Mock).mockReturnValue({
+  it('로그인이 안되어 있을 시 로그인 페이지 리디렉션 테스트', () => {
+    (useAuthStore as unknown as jest.Mock).mockReturnValue({
       isLoggedIn: false,
     });
     render(<CreateRoom />);
